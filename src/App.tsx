d216ea2c9fb607465606715473455166f3baaab6
Leandro Del Arco <leandrodelarco@outlook.com>
2024-03-20 21:45:12 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import { Excalidraw } from "@excalidraw/excalidraw";
+
+function App() {
+  return (
+    <>
+      <div style={{ height: "90vh" }}>
+        <Excalidraw />
+      </div>
+    </>
+  );
+}
+
+export default App;
